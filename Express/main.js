8f07e7a584a423e974053d27bad4fc747c780f9f
tkdlxm457 <tkdlxm457@naver.com>
2021-08-09 21:45:55 +0900

    Express

---
+++ Express/main.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var compression = require('compression')
+var topicRouter = require('./routes/topic');
+var indexRouter = require('./routes/index');
+var helmet = require('helmet');
+app.use(helmet()); 
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(compression());
+app.get('*', function(request, response, next){
+  fs.readdir('./data', function(error, filelist){
+    request.list = filelist;
+    next();
+  });
+});
+
+app.use('/', indexRouter);
+app.use('/topic', topicRouter);
+ 
+app.use(function(req, res, next) {
+  res.status(404).send('Sorry cant find that!');
+});
+ 
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+});
+ 
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!')
+});
---
+++ Express/password.js
@@ -0,0 +1,4 @@
+module.exports = {
+    id: 'kkk',
+    password: '1111'
+}
